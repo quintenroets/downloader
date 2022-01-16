54aa4c4748838d9e0b320e21b57ee70db9bb44da
Quinten <quinten.roets@gmail.com>
2022-01-16 15:29:57 +0100

    use single quotes

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
-NAME = "downloader"
+NAME = 'downloader'
 
 
 def read(filename):
     try:
         with open(filename) as fp:
-            content = fp.read().split("\n")
+            content = fp.read().split('\n')
     except FileNotFoundError:
         content = []
     return content
 
 setup(
-    author="Quinten Roets",
-    author_email="quinten.roets@gmail.com",
+    author='Quinten Roets',
+    author_email='quinten.roets@gmail.com',
     description='',
     name=NAME,
     version='1.0',
     packages=find_packages(),
-    install_requires=read("requirements.txt"),
+    install_requires=read('requirements.txt'),
     entry_points={
-        "console_scripts": [
-            "download = downloader.main:main"
+        'console_scripts': [
+            'download = downloader.main:main'
             ]
         },
 )
