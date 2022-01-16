eb8beb94cdb435a4105cf46174a116ab25619ac2
Quinten <quinten.roets@gmail.com>
2022-01-16 14:06:34 +0100

    refactor to retry library instead of writing retry function

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,112 +1,113 @@
 import requests
 import shutil
 import time
+import urllib3
 import urllib.parse
 
 from plib import Path
+from retry import retry
 from tqdm import tqdm
 from tqdm.utils import CallbackIOWrapper
 
+TRIES = 5
+
 
 class Downloader:
-    def __init__(self, url, dest=None, folder=None, session=None, headers=None, progress_callback=None, retries=4, timeout=10,
-                 overwrite_identical_size=True
-                 ):
+    def __init__(
+            self, url, dest=None, folder=None, session=None, headers=None, 
+            progress_callback=None, timeout=10, skip_same_size=False
+        ):
         self.url = url
-        dest = dest or Downloader.url_name(url) or "download"
-        if folder:
-            dest = Path(folder) / dest
-        self.dest = Path(dest)
+        self.dest = self.prepare_dest(dest, folder)
+        self.session = self.prepare_session(session, headers)
         
-        self.session = session or requests.Session()
-        self.session.stream = True
+        self.retry = -1
+        self.timeout = timeout
+        self.progress_callback = progress_callback or (lambda p: None)
+        self.skip_same_size = skip_same_size
         
-        user_agent = (
+    def prepare_session(self, session, headers):
+        session = session or requests.Session()
+        browser = (
             'Mozilla/5.0 (X11; Linux x86_64) '
             'AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/85.0.4183.83 Safari/537.36'
         )
-        check_time = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime(self.dest.mtime))
+        download_time = time.gmtime(self.dest.mtime)
+        download_time = time.strftime('%a, %d %b %Y %H:%M:%S GMT', download_time)
+        headers = (headers or {}) | {
+            'User-Agent': browser,
+            'If-Modified-Since': download_time
+        }
+        session.headers.update(headers)
+        return session
         
-        headers = headers or {}
-        headers['User-Agent'] = user_agent  # browser user agent often required
-        headers['If-Modified-Since'] = check_time
-        self.session.headers.update(headers)
-        self.not_modified = False
+    def prepare_dest(self, dest, folder):
+        if dest is None:
+            url_path = urllib.parse.urlparse(self.url).path
+            name = urllib.parse.unquote(url_path).split('/')[-1]
+            dest = name or 'download'
+            
+        dest = Path(folder) / dest if folder else Path(dest)
+        return dest
         
-        self.retries = retries
-        self.retry = 0
-        self.timeout = timeout
-        self.progress_callback = progress_callback or (lambda p: None)
-        self.overwrite_identical_size = overwrite_identical_size
+    @retry(
+        (requests.exceptions.RequestException, urllib3.exceptions.HTTPError), 
+        tries=TRIES
+    )
+    def download(self):
+        self.retry += 1
+        headers = {'Range': f'bytes={self.temp_dest.size}-'}
+        stream = self.session.get(self.url, timeout=self.timeout, stream=True, headers=headers)
+        if stream.status_code != 304:
+            self.download_modified(stream)
+            
+    def download_modified(self, stream):        
+        if stream.status_code == 416:  # range not satifiable or supported
+            stream = self.session.get(self.url, timeout=self.timeout, stream=True)
+        elif 'Content-Range' in stream.headers:
+            start = int(stream.headers['Content-Range'].split('bytes ')[1].split('-')[0])
+            if self.temp_dest.size > start:
+                self.temp_dest.unlink()
+                stream = self.session.get(self.url, timeout=self.timeout, stream=True)
+        else:
+            assert stream.ok
+            
+        skip = self.skip_same_size and self.dest.size == int(stream.headers['Content-Length'])
+        if not skip:
+            self.start_download(stream)
+            
+    def start_download(self, stream):
+        total = int(stream.headers['Content-Length'])  # length that still needs to be received
+        progress = tqdm(total=total, desc=self.description, unit='B', unit_scale=True, unit_divisor=1024)
         
-    @staticmethod
-    def url_name(url):
-        url_path = urllib.parse.urlparse(url).path
-        name = urllib.parse.unquote(url_path).split("/")[-1]
-        return name
+        def progres_callback(value):
+            progress.update(value)
+            self.progress_callback(value / total)
+        
+        stream_raw = CallbackIOWrapper(progres_callback, stream.raw)
+        
+        chunk_size = self.truncate(total // 10, 1 * 1024, 128 * 1024)
+        with progress, self.temp_dest.open('ab') as fp:
+            shutil.copyfileobj(stream_raw, fp, length=chunk_size)
+        
+        self.temp_dest.rename(self.dest)
         
     @property
     def temp_dest(self):
-        return self.dest.with_suffix(self.dest.suffix + ".part")
-    
-    def download(self):
-        succes = False
+        return self.dest.with_suffix(self.dest.suffix + '.part')
         
-        while not succes and self.retry <= self.retries:
-            try:
-                self.try_download()
-            except requests.exceptions.RequestException:
-                self.retry += 1
-            else:
-                succes = True
-                    
-        if succes:
-            if not self.not_modified:
-                self.temp_dest.rename(self.dest)
-        else:
-            raise requests.exceptions.RequestException
-        
-    def try_download(self):
-        self.session.headers["Range"] = f"bytes={self.temp_dest.size}-"
-        stream = self.session.get(self.url, timeout=self.timeout)
-        
-        if stream.status_code == 304:
-            self.not_modified = True
-        elif stream.status_code == 416: # range not satifiable or supported
-            self.session.headers.pop("Range")
-            stream = self.session.get(self.url, timeout=self.timeout)
-            start = 0
-        elif 'Content-Range' in stream.headers:
-            start = int(stream.headers["Content-Range"].split("bytes ")[1].split("-")[0])
-        elif stream.ok:
-            start = 0
-        else:
-            raise requests.exceptions.RequestException
-        
-        if not self.not_modified:
-            self.not_modified = not self.overwrite_identical_size and self.dest.size == int(stream.headers["Content-Length"])
-        
-        if not self.not_modified:
-            self.start_download(start, stream)
-            
-    def start_download(self, start, stream):
-        if self.temp_dest.size > start:
-            self.temp_dest.unlink()
-            start = 0
-        
-        total = int(stream.headers["Content-Length"])  # length that still needs to be received        
-        chunk_size = min(total // 10, 128 * 1024)  # max 128 KB
-        chunk_size = max(chunk_size, 1)  # min 1 KB
-        desc = f"Downloading {self.dest.name}"
+    @property
+    def description(self):
+        desc = f'Downloading {self.dest.name}'
         if self.retry > 0:
-            desc += f" (retry {self.retry}/{self.retries}"
-                        
-        with tqdm(total=total, desc=desc, unit="B", unit_scale=True, unit_divisor=1024) as progress:
-            def callback(value):
-                progress.update(value)
-                self.progress_callback(value / progress.total)
-                
-            stream_raw = CallbackIOWrapper(callback, stream.raw)
-            with self.temp_dest.open("ab") as fp:
-                shutil.copyfileobj(stream_raw, fp, length=chunk_size)
+            desc += f' (retry {self.retry}/{TRIES - 1}'
+        return desc
+    
+    @staticmethod
+    def truncate(value, min_value, max_value):
+        if value < min_value:
+            value = min_value
+        elif value > max_value:
+            value = max_value
+        return value
