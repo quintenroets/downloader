d9b380e5e650aee1fcb10767e9231e7b4ab74931
Quinten <quinten.roets@gmail.com>
2022-01-27 21:12:51 +0100

    fix import of downloader

--- downloader/main.py
+++ downloader/main.py
@@ -1,16 +1,20 @@
 import argparse
 
-from . import downloader
+import downloader
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download url to file with opention to retry on errors and resume from partial downloads')
-    parser.add_argument('url', help='The url to download')
-    parser.add_argument('dest', nargs='?', help='The local file to save the download', default=None)
+    parser = argparse.ArgumentParser(
+        description="Download url to file with opention to retry on errors and resume from partial downloads"
+    )
+    parser.add_argument("url", help="The url to download")
+    parser.add_argument(
+        "dest", nargs="?", help="The local file to save the download", default=None
+    )
     args = parser.parse_args()
-    
+
     downloader.download(args.url, args.dest)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
