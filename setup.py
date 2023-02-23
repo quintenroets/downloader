f5818cc1db8cacf50f4868323aa6bf6fdffd86e9
Quinten <quinten.roets@gmail.com>
2023-02-23 16:32:26 -0500

    publish on pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-NAME = "downloader"
-
-
-def read(filename):
-    try:
-        with open(filename) as fp:
-            content = fp.read().split("\n")
-    except FileNotFoundError:
-        content = []
-    return content
-
-
-setup(
-    author="Quinten Roets",
-    author_email="quinten.roets@gmail.com",
-    description="",
-    name=NAME,
-    version="1.0",
-    packages=find_packages(),
-    install_requires=read("requirements.txt"),
-    entry_points={"console_scripts": ["download = downloader.main:main"]},
-)
+if __name__ == "__main__":
+    setup()
