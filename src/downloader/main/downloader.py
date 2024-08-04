8f5285efdbae5a07421d946fe1ee5c9ea0e72db4
Quinten <quinten.roets@gmail.com>
2024-08-04 10:29:32 -0700

    use ruff best practices

--- src/downloader/main/downloader.py
+++ src/downloader/main/downloader.py
@@ -2,7 +2,8 @@ import shutil
 import time
 import urllib.parse
 from collections.abc import Callable
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import TypeVar
 
 import dateutil.parser
 import requests
@@ -14,129 +15,137 @@ from tqdm.utils import CallbackIOWrapper
 from .progress import UIProgress
 
 TRIES = 5
+CONTENT_MODIFIED = 304
+NOT_SATISFIABLE = 416  # range not satisfiable or supported
+T = TypeVar("T", int, float)
 
 
 @dataclass
 class Downloader:
     url: str
-    dest: Path | str | None = None
-    folder: Path | str = None
-    session: requests.Session | None = None
-    headers: dict | None = None
-    retry_amount: int = -1
+    target: Path = field(default_factory=Path)
+    folder: Path | str = ""
+    session: requests.Session = field(default_factory=requests.Session)
+    headers: dict[str, str] | None = None
+    number_of_retries: int = -1
     timeout: int = 10
-    progress_callback: Callable | None = None
+    progress_callback: Callable[[float], None] | None = None
     skip_same_size: bool = False
     set_time: bool = True  # set modified time to modified time on server
 
     def __post_init__(self) -> None:
-        self.dest = self.prepare_dest(self.dest, self.folder)
-        self.session = self.prepare_session(self.session, self.headers)
+        self.prepare_destination()
+        self.prepare_session()
 
-    def prepare_session(self, session, headers):
-        session = session or requests.Session()
+    def prepare_session(self) -> None:
         browser = (
             "Mozilla/5.0 (X11; Linux x86_64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
             "Chrome/85.0.4183.83 Safari/537.36"
         )
-        download_time = time.gmtime(self.dest.mtime)
-        download_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT", download_time)
-        headers = (headers or {}) | {
+        download_time = time.gmtime(self.target.mtime)
+        parsed_download_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT", download_time)
+        default_headers = {
             "User-Agent": browser,
-            "If-Modified-Since": download_time,
+            "If-Modified-Since": parsed_download_time,
         }
-        session.headers.update(headers)
-        return session
+        headers = (self.headers or {}) | default_headers
+        self.session.headers.update(headers)
 
-    def prepare_dest(self, dest, folder):
-        if dest is None:
+    def prepare_destination(self) -> None:
+        target: Path | str
+        if self.target.parts:
+            target = self.target
+        else:
             url_path = urllib.parse.urlparse(self.url).path
             name = urllib.parse.unquote(url_path).split("/")[-1]
-            dest = name or "download"
+            target = name or "download"
 
-        dest = Path(folder) / dest if folder else Path(dest)
-        return dest
+        self.target = Path(self.folder) / target if self.folder else Path(target)
 
     @retry(
         (requests.exceptions.RequestException, urllib3.exceptions.HTTPError),
         tries=TRIES,
     )
     def download(self) -> None:
-        self.retry_amount += 1
-        headers = {"Range": f"bytes={self.temp_dest.size}-"}
+        self.number_of_retries += 1
+        headers = {"Range": f"bytes={self.temporary_target.size}-"}
         if "If-Modified-Since" in self.session.headers:
             self.session.headers.pop("If-Modified-Since")
         stream = self.session.get(
-            self.url, timeout=self.timeout, stream=True, headers=headers
+            self.url,
+            timeout=self.timeout,
+            stream=True,
+            headers=headers,
         )
-        if stream.status_code != 304:
+        if stream.status_code != CONTENT_MODIFIED:
             self.download_modified(stream)
 
-    def download_modified(self, stream) -> None:
-        if stream.status_code == 416:  # range not satifiable or supported
+    def download_modified(self, stream: requests.Response) -> None:
+        if stream.status_code == NOT_SATISFIABLE:
             stream = self.session.get(self.url, timeout=self.timeout, stream=True)
         elif "Content-Range" in stream.headers:
             start = int(
-                stream.headers["Content-Range"].split("bytes ")[1].split("-")[0]
+                stream.headers["Content-Range"].split("bytes ")[1].split("-")[0],
             )
-            if self.temp_dest.size > start:
-                self.temp_dest.unlink()
+            if self.temporary_target.size > start:
+                self.temporary_target.unlink()
                 stream = self.session.get(self.url, timeout=self.timeout, stream=True)
-        else:
-            assert stream.ok
+        elif not stream.ok:
+            message = f"Server returned status code {stream.status_code}."
+            raise RuntimeError(message)
 
-        skip = self.skip_same_size and self.dest.size == int(
-            stream.headers["Content-Length"]
+        skip = self.skip_same_size and self.target.size == int(
+            stream.headers["Content-Length"],
         )
         if not skip:
             self.start_download(stream)
 
-    def start_download(self, stream) -> None:
+    def start_download(self, stream: requests.Response) -> None:
         total = (
             int(
-                stream.headers["Content-Length"]
+                stream.headers["Content-Length"],
             )  # length that still needs to be received
             if "Content-Length" in stream.headers
             else int(stream.headers["Content-Range"].split("/")[-1])
         )
 
-        progress = UIProgress(self.description, total=total + self.dest.size)
+        progress = UIProgress(self.description, total=total + self.target.size)
         # downloaded size added to todo and done
 
-        def progres_callback(value) -> None:
+        def progres_callback(value: float) -> None:
             progress.advance(value)
             if self.progress_callback is not None:
                 self.progress_callback(value / total)
 
-        progres_callback(self.dest.size)  # already downloaded part is progress
+        progres_callback(self.target.size)  # already downloaded part is progress
 
         stream_raw = CallbackIOWrapper(progres_callback, stream.raw)
 
         chunk_size = self.truncate(total // 10, 1 * 1024, 128 * 1024)
-        with progress, self.temp_dest.open("ab") as fp:
+        with progress, self.temporary_target.open("ab") as fp:
             shutil.copyfileobj(stream_raw, fp, length=chunk_size)
 
-        self.temp_dest.rename(self.dest)
+        self.temporary_target.rename(self.target)
         modified_key = "last-modified"
         if self.set_time and modified_key in stream.headers:
             server_mtime = stream.headers[modified_key]
-            self.dest.mtime = dateutil.parser.parse(server_mtime).timestamp()
+            self.target.mtime = dateutil.parser.parse(server_mtime).timestamp()
 
     @property
-    def temp_dest(self):
-        return self.dest.with_suffix(self.dest.suffix + ".part")
+    def temporary_target(self) -> Path:
+        return self.target.with_suffix(self.target.suffix + ".part")
 
     @property
-    def description(self):
+    def description(self) -> str:
         return (
-            f"[retry {self.retry_amount}/{TRIES - 1}] {self.dest.name}"
-            if self.retry_amount > 0
-            else self.dest.name
+            f"[retry {self.number_of_retries}/{TRIES - 1}] {self.target.name}"
+            if self.number_of_retries > 0
+            else self.target.name
         )
 
     @staticmethod
-    def truncate(value, min_value, max_value):
+    def truncate(value: T, min_value: T, max_value: T) -> T:
         if value < min_value:
             value = min_value
         elif value > max_value:
--- src/downloader/main/main.py
+++ src/downloader/main/main.py
@@ -1,31 +1,38 @@
 import threading
+from collections.abc import Iterable
+from typing import Any
 
 from superpathlib import Path
 
 from .downloader import Downloader
 
 
-def download(url: str, dest: Path | None = None, **kwargs):
-    downloader = Downloader(url, dest=dest, **kwargs)
+def download(url: str, target: Path | None = None, **kwargs: Any) -> Path:
+    if target is None:
+        target = Path()
+    downloader = Downloader(url, target=target, **kwargs)
     downloader.download()
-    return downloader.dest
+    return downloader.target
 
 
-def get(*urls, **kwargs):
+def get(*urls: Any, **kwargs: Any) -> list[bytes]:
     """Get the content of urls and cache it to a local file.
 
     Useful to speed up the process when the same url is requested
     multiple times
     """
     folder = Path.HOME / ".cache" / "downloader"
-    urls = {u: folder / "_".join(u.split("/")) for u in urls}
-    dest_paths = download_urls(urls, **kwargs)
+    urls_mapping = {u: folder / "_".join(u.split("/")) for u in urls}
+    dest_paths = download_urls(urls_mapping, **kwargs)
     return [dest_path.byte_content for dest_path in dest_paths]
 
 
-def download_urls(urls, **kwargs):
+def download_urls(
+    urls: dict[str, Path | str] | Iterable[str],
+    **kwargs: Any,
+) -> list[Path]:
     downloaders = (
-        [Downloader(url, dest, **kwargs) for url, dest in urls.items()]
+        [Downloader(url, Path(target), **kwargs) for url, target in urls.items()]
         if isinstance(urls, dict)
         else [Downloader(url, **kwargs) for url in urls]
     )
@@ -34,4 +41,4 @@ def download_urls(urls, **kwargs):
         t.start()
     for t in threads:
         t.join()
-    return [d.dest for d in downloaders]
+    return [downloader.target for downloader in downloaders]
